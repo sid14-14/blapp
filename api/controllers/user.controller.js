ce879857291b1d47320e3224c0d66e8a19fd8d7a
sid14-14 <sidd.work.mail@gmail.com>
2024-08-26 19:47:44 +0530

    completed 'update user profile page' functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -34,6 +34,7 @@ export const updateUser = async (req, res, next) => {
         errorHandler(400, 'Username can only contain letters and numbers')
       );
     }
+  }
     try {
       const updatedUser = await User.findByIdAndUpdate(
         req.params.userId,
@@ -52,5 +53,4 @@ export const updateUser = async (req, res, next) => {
     } catch (error) {
       next(error);
     }
-  }
 };
