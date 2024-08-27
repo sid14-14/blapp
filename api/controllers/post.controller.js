36ab982be37c8d269902c421a9be6aac495156b4
sid14-14 <sidd.work.mail@gmail.com>
2024-08-27 19:27:39 +0530

    added update post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -82,3 +82,26 @@ export const deletepost = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updatepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this post'));
+  }
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: { //we change the vals based on the paras we send, in order to maintain security and prevent user from changing the userid we mention only things we want to change
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image,
+        },
+      },
+      { new: true } //to get the new result
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+};
