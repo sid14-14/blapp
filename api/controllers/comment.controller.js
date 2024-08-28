ac60a3e4e22c93ab113371e83ce46aac2e281d1e
sid14-14 <sidd.work.mail@gmail.com>
2024-08-28 18:54:39 +0530

    added like functionality to the comment section

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -33,3 +33,24 @@ export const getPostComments = async (req, res, next) => {
     next(error);
   }
 };
+
+export const likeComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, 'Comment not found'));
+    }
+    const userIndex = comment.likes.indexOf(req.user.id); //if the user.id is inside the likes array
+    if (userIndex === -1) { //if user not exist we add him
+      comment.numberOfLikes += 1;
+      comment.likes.push(req.user.id);
+    } else { //else we remove him
+      comment.numberOfLikes -= 1;
+      comment.likes.splice(userIndex, 1);
+    }
+    await comment.save();
+    res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
